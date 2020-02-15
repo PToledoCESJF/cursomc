efce40130377bd4bf938d7784ca4a4a9b484eb1a
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-02-14 21:50:02 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/paulotoledo/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.paulotoledo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulotoledo.cursomc.domain.Categoria;
+import com.paulotoledo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}

a8b8b21e9172ae93a75d07a9c32c9810ccb3ddf8
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-03-09 20:04:20 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/paulotoledo/cursomc/services/CategoriaService.java
+++ src/main/java/com/paulotoledo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.paulotoledo.cursomc.domain.Categoria;
+import com.paulotoledo.cursomc.dto.CategoriaDTO;
 import com.paulotoledo.cursomc.repositories.CategoriaRepository;
 import com.paulotoledo.cursomc.services.exceptions.DataIntegrityException;
 import com.paulotoledo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 	

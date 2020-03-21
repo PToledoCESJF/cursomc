299dc5166d6e2df23e19d651d01acc5ac136514b
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-03-21 13:05:27 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/paulotoledo/cursomc/services/CidadeService.java
@@ -0,0 +1,20 @@
+package com.paulotoledo.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulotoledo.cursomc.domain.Cidade;
+import com.paulotoledo.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+
+	public List<Cidade> findByEstado(Integer estadoId) {
+		return repo.findCidades(estadoId);
+	}
+}
